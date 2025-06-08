1f21fa2043b78c941b530ec1feb2a59726433b79
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-06-07 18:23:15 -0600

    Moved BottomSheet code into Agenda

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,14 @@
-import React, { useMemo } from "react";
-import { Platform, StyleSheet, View, Text } from "react-native";
-import { useAnimatedReaction, useSharedValue } from "react-native-reanimated";
+import React from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
 
 import { CalendarProvider } from "@/components/CalendarContext";
 import Header from "@/components/Header";
 import WeekPager from "@/components/WeekPager";
 import MonthPager from "@/components/MonthPager";
-import Event from "@/components/Event";
-import Activity from "@/components/Activity";
+import Agenda from "@/components/Agenda";
 
 export default function App() {
   return (
@@ -27,15 +25,7 @@ const CalendarContent = () => {
   const bottomSheetTranslationY = useSharedValue(0)
   const selectedDatePosition = useSharedValue(0)
   let paddingTop = Platform.OS === 'android' ? 0 : insets.top
-  const initialCalendarBottom = (47 * 6) + paddingTop + 52
   const calendarBottom = useSharedValue((47 * 6) + paddingTop + 52)
-
-  const snapPoints = useMemo(() => [667 - initialCalendarBottom, 667 - initialCalendarBottom + 235], []);
-
-  useAnimatedReaction(() => { return bottomSheetTranslationY.value },
-    (curr, prev) => {
-      // console.log(curr)
-    })
 
   return (
     <View style={{ flex: 1, backgroundColor: '#F8F8F8', paddingTop: paddingTop, paddingBottom: insets.bottom }}>
@@ -52,60 +42,11 @@ const CalendarContent = () => {
         calendarBottom={calendarBottom}
         selectedDatePosition={selectedDatePosition}
       />
-      <BottomSheet
-        snapPoints={snapPoints}
-        animatedPosition={bottomSheetTranslationY}
-        enableOverDrag={false}
-        enablePanDownToClose={false}
-        enableDynamicSizing={false}
-        animateOnMount={false}
-        style={styles.bottomSheet}
-      >
-        <BottomSheetScrollView>
-          <View style={styles.section}>
-            <Text style={styles.sectionHeadingText}>{"Schedule"}</Text>
-            <Event />
-            <Event />
-            <Event />
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.sectionHeadingText}>{"Assignments"}</Text>
-            <Activity />
-            <Activity />
-            <Activity />
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.sectionHeadingText}>{"Tasks"}</Text>
-            <Activity />
-            <Activity />
-            <Activity />
-          </View>
-        </BottomSheetScrollView>
-      </BottomSheet>
+      <Agenda
+        bottomSheetTranslationY={bottomSheetTranslationY}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  bottomSheet: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    position: 'absolute',
-    zIndex: 3,
-  },
-  section: {
-    marginHorizontal: 20,
-    marginVertical: 10,
-  },
-  sectionHeadingText: {
-    fontSize: 22,
-    fontWeight: '500',
-    marginBottom: 4
-  }
-});
+const styles = StyleSheet.create({});
---
+++ components/Agenda.tsx
@@ -0,0 +1,76 @@
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import React, { useMemo } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SharedValue } from 'react-native-reanimated';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
+
+import Event from "@/components/Event";
+import Activity from "@/components/Activity";
+
+type AgendaProps = {
+  bottomSheetTranslationY: SharedValue<number>
+}
+
+export default function Agenda({ bottomSheetTranslationY }: AgendaProps) {
+  const insets = useSafeAreaInsets()
+  let paddingTop = Platform.OS === 'android' ? 0 : insets.top
+  const initialCalendarBottom = (47 * 6) + paddingTop + 52
+  const snapPoints = useMemo(() => [667 - initialCalendarBottom, 667 - initialCalendarBottom + 235], []);
+
+  return (
+    <BottomSheet
+      snapPoints={snapPoints}
+      animatedPosition={bottomSheetTranslationY}
+      enableOverDrag={false}
+      enablePanDownToClose={false}
+      enableDynamicSizing={false}
+      animateOnMount={false}
+      style={styles.bottomSheet}
+    >
+      <BottomSheetScrollView>
+        <View style={styles.section}>
+          <Text style={styles.sectionHeadingText}>{"Schedule"}</Text>
+          <Event />
+          <Event />
+          <Event />
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionHeadingText}>{"Assignments"}</Text>
+          <Activity />
+          <Activity />
+          <Activity />
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionHeadingText}>{"Tasks"}</Text>
+          <Activity />
+          <Activity />
+          <Activity />
+        </View>
+      </BottomSheetScrollView>
+    </BottomSheet>
+  )
+}
+
+const styles = StyleSheet.create({
+  bottomSheet: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    position: 'absolute',
+    zIndex: 3,
+  },
+  section: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  sectionHeadingText: {
+    fontSize: 22,
+    fontWeight: '500',
+    marginBottom: 4
+  }
+})
