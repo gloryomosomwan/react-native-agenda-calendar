7ad07694c18939261f00b9dbb30f063f079debca
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-06-09 17:48:10 -0600

    Cleanup

--- utils/themes.ts
+++ utils/themes.ts
@@ -9,24 +9,23 @@ type theme = {
   warning: string,
 }
 
+export const lightTheme: theme = {
+  primary: 'white',
+  secondary: '#F8F8F8',
+  tertiary: '#bcbec4',
+  accent: "#007bff",
+  text: 'black',
+  inverseText: 'white',
+  danger: 'red',
+  warning: 'gold',
+}
+
 export const darkTheme: theme = {
   primary: 'black',
   secondary: '#151616',
   tertiary: '#525252',
   accent: '#007bff',
   text: 'white',
-  inverseText: 'white',
-  danger: 'red',
-  warning: 'gold',
-}
-
-export const lightTheme: theme = {
-  primary: 'white',
-  secondary: '#F8F8F8',
-  // darkgrey: 'grey',
-  tertiary: '#bcbec4',
-  accent: "#007bff",
-  text: 'black',
   inverseText: 'white',
   danger: 'red',
   warning: 'gold',
